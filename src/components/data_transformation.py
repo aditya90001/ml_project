899b73f7794a2ae3e001fa5ed25c75f910d126ef
aditya9001 <adityas02903@gmail.com>
2026-06-09 21:42:44 +0530

    hyper parameter done

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -13,7 +13,7 @@ from src.logger import logging
 import os
 
 from src.utils import save_object
-## this is used to store the files 
+
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
@@ -88,10 +88,10 @@ class DataTransformation:
             target_column_name="math_score"
             numerical_columns = ["writing_score", "reading_score"]
 
-            input_feature_train_df=train_df.drop(target_column_name, axis=1)
+            input_feature_train_df = train_df.drop(columns=[target_column_name])
             target_feature_train_df=train_df[target_column_name]
 
-            input_feature_test_df=test_df.drop(target_column_name, axis=1)
+            input_feature_test_df = test_df.drop(columns=[target_column_name])
             target_feature_test_df=test_df[target_column_name]
 
             logging.info(
